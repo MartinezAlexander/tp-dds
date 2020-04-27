37c4b61d83ea09958dddc01e7670155d8a448075
josebruzzoni <bruzzonijose@gmail.com>
2020-04-27 18:38:57 -0300

    Agregado el metodo y test del integrante 1

--- src/main/java/pruebaConcepto/DummyDesign.java
+++ src/main/java/pruebaConcepto/DummyDesign.java
@@ -4,4 +4,8 @@ public class DummyDesign {
 	public static int integrante4() {
 		return 4;
 	}
+	public static int integrante1() {
+		return 1;
+	}
+	
 }
--- src/test/java/testConcepto/TestDummyDesign.java
+++ src/test/java/testConcepto/TestDummyDesign.java
@@ -7,8 +7,13 @@ import org.junit.Test;
 public class TestDummyDesign {
 	
 	@Test
-	public void test() {
+	public void testIntegrante4() {
 		Assert.assertEquals(DummyDesign.integrante4(),4);
+	}
+	
+	@Test
+	public void testIntegrante1() {
+		Assert.assertEquals(DummyDesign.integrante1(), 1);
 	}
 
 }
