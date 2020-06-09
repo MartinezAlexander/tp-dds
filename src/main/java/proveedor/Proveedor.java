1b3b40e8a7bcff83e6b9e17e067b51e9364f4fc8
josebruzzoni <bruzzonijose@gmail.com>
2020-06-09 16:25:21 -0300

    Issue proveedor arreglado

--- src/main/java/proveedor/Proveedor.java
+++ src/main/java/proveedor/Proveedor.java
@@ -1,17 +1,43 @@
 package proveedor;
 
-public abstract class Proveedor {
+public class Proveedor {
 	private String direccionPostal;
 	
-	public abstract String nombre();
-	public abstract int identificacion();
+	private String razonSocial;
+	private int cuitCuil;
 
-	public Proveedor(String direccionPostal) {
+	private String nombre;
+	private String apellido;
+	private int dni;
+
+	public Proveedor(String direccionPostal, String nombre, String apellido, int dni, String razonSocial, int cuitCuil) {
 		this.direccionPostal = direccionPostal;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.cuitCuil = cuitCuil;
+		this.razonSocial = razonSocial;
 	}
-
+	
 	public String getDireccionPostal() {
 		return direccionPostal;
 	}
+
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	
+	public String getNombreYApellido() {
+		return nombre + " " + apellido;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public int getCuitCuil() {
+		return cuitCuil;
+	}
+	
 }
 
