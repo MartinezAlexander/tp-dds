c94b55e9c9b7d7ddda3378ecd82cf4d4964a08b2
josebruzzoni <bruzzonijose@gmail.com>
2020-06-15 13:22:22 -0300

    cambio el forEach ese que estaba mal en presupuesto

--- src/main/java/presupuestos/Presupuesto.java
+++ src/main/java/presupuestos/Presupuesto.java
@@ -1,6 +1,7 @@
 package presupuestos;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -10,12 +11,12 @@ import proveedor.Proveedor;
 
 public class Presupuesto {
 	private Proveedor proveedor;
-	private List<ItemOperacion> items;
+	private ArrayList<ItemOperacion> items;
 	private BigDecimal cotizacion;
 	private DocumentoComercial documentoComercial;
 	private Date fecha;
 	
-	public Presupuesto(Proveedor proveedor, List<ItemOperacion> items, DocumentoComercial documentoComercial, Date fecha) {
+	public Presupuesto(Proveedor proveedor, ArrayList<ItemOperacion> items, DocumentoComercial documentoComercial, Date fecha) {
 		this.proveedor = proveedor;
 		this.items = items;
 		this.documentoComercial = documentoComercial;
@@ -23,7 +24,9 @@ public class Presupuesto {
 	}
 	
 	public BigDecimal getCotizacion() {
-		return this.items.forEach((item) => {item.getValor()} ).sum(); //pseudocodigo, no me acuerdo como se usa el foreach
+		return this.items.stream()
+			      .map(i -> i.getValor())
+			      .sum();
 	}
 	
 	
