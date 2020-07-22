92180083fc6cf3da63da299e6fab3ef8cdd1e3f8
Alexander Martinez <alexandercristianmartinez@gmail.com>
2020-07-22 19:16:46 -0300

    Agrego una tarea programada para que valide los egresos cada cierto tiempo

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,9 @@
 package main;
+import java.util.Date;
 import operaciones.ValidadorDeOperaciones;
 
 public class Main {
-
 	public static void main(String[] args) {
-		ValidadorDeOperaciones.validarOperacionesPendientes();
+		ValidadorDeOperaciones.getInstance().programarValidacionOperacionesPendientes(new Date(), 1000);
 	}
-
 }
--- src/main/java/operaciones/ValidadorDeOperaciones.java
+++ src/main/java/operaciones/ValidadorDeOperaciones.java
@@ -1,13 +1,33 @@
 package operaciones;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class ValidadorDeOperaciones {
+	
+	private static ValidadorDeOperaciones instance = null;
+	private Timer timer;
+	private TimerTask timerTask;
+	
+	private ValidadorDeOperaciones() {
+		timer = new Timer();
+		timerTask = new TimerTask() {
+			@Override
+			public void run() {
+				RepositorioOperaciones.obtenerOperacionesPendientesDeValidacion()
+						.forEach(OperacionDeEgreso::realizarValidacion);
+			}
+		};
+	}
+	
+    public static ValidadorDeOperaciones getInstance(){
+        if (instance == null) instance = new ValidadorDeOperaciones();
+        return instance;
+    }
 
-    public static void validarOperacionesPendientes(){
-        RepositorioOperaciones.obtenerOperacionesPendientesDeValidacion()
-                .forEach(OperacionDeEgreso::realizarValidacion);
+    public void programarValidacionOperacionesPendientes(Date fechaInicio, int cadaCuantoTiempo){
+    	timer.schedule(timerTask, fechaInicio, cadaCuantoTiempo);
     }
 }
